07ef4ac153670212497972de3b89879810285b3d
Gitea <gitea@fake.local>
2022-06-16 16:04:59 +0800

    first commit

---
+++ src/com/test/package01/HelloGroovy.groovy
@@ -0,0 +1,9 @@
+package com.test.package01
+
+println "Hello Groovy 1"
+println 'Hello Groovy 2'
+println("Hello Groovy 3");
+println("Hello Groovy 4")
+println('Hello Groovy 5')
+System.out.println("Hello Groovy 6");
+
